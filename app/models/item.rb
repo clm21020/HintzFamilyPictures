5216803f8fb7e1e2bf755d6ec9278da75e9356b2
Cassie Montoya <clm21020@gmail.com>
2015-10-24 10:40:24 -0700

    Add Item model

---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Item < ActiveRecord::Base
+	validates :title, presence: true
+end
