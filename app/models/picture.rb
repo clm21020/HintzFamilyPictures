4b2de6ba65c55d9298e3b8f5c8f1988de2c57e20
Cassie Montoya <clm21020@gmail.com>
2015-10-24 10:49:10 -0700

    Add Picture model

---
+++ app/models/picture.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: pictures
+#
+#  id         :integer          not null, primary key
+#  url        :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Picture < ActiveRecord::Base
+	validates :url, presence: true
+end
