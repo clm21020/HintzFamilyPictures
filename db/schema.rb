0e093c78e1af63dea4f1525c54ba273d09a9793d
Cassie Montoya <clm21020@gmail.com>
2015-10-24 10:47:13 -0700

    Add Pictures table to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024172350) do
+ActiveRecord::Schema.define(version: 20151024174115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.text     "url",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
