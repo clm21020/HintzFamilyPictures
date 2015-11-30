f13bb4a17f050fc29b0d2ecebd99c7ffff45a0a4
Cassie Montoya <clm21020@gmail.com>
2015-11-29 23:17:09 -0800

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  admin           :boolean          default(FALSE), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+	validates :username, :email, :session_token, presence: true
+	validates :username, :email, :session_token, uniqueness: true
+	validates :password_digest, presence: { message: "Password can't be blank" }
+	validates :password, length: { minimum: 6, nil: true }
+
+	attr_reader :password
+
+	after_initialize :ensure_session_token
+
+	def self.find_by_credentials(identifier, password)
+		user = user.find_by_username(identifier) || user.find_by_email(identifier)
+		return nil unless user && user.valid_password(password)
+		user
+	end
+
+	def password=(password)
+		@password = password
+		self.password_digest = BCrypt::Password.create(password)
+	end
+
+	def valid_password?(password)
+		BCrypt::Password.new(self.password_digest).is_password?(password)
+	end
+
+	def reset_session_token!
+		self.session_token = SecureRandom::urlsafe_base64
+		self.save!
+		self.session_token
+	end
+
+	private
+	def ensure_session_token
+		self.session_token ||= SecureRandom::urlsafe_base64
+	end
+end
