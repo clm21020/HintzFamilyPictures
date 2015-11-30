b08ba358e44739544cf0baa4ca5803af594707f3
Cassie Montoya <clm21020@gmail.com>
2015-11-29 23:16:40 -0800

    Add Users table to the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116052237) do
+ActiveRecord::Schema.define(version: 20151130044458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,5 +42,19 @@ ActiveRecord::Schema.define(version: 20151116052237) do
   end
 
   add_index "pictures", ["item_id"], name: "index_pictures_on_item_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",                        null: false
+    t.string   "email",                           null: false
+    t.string   "password_digest",                 null: false
+    t.string   "session_token",                   null: false
+    t.boolean  "admin",           default: false, null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
