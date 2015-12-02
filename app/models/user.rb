bcf626bd1766578ee56334d124c784100e1c4337
Cassie Montoya <clm21020@gmail.com>
2015-12-01 21:27:57 -0800

    Fix typos in User model's find_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,8 @@ class User < ActiveRecord::Base
 	after_initialize :ensure_session_token
 
 	def self.find_by_credentials(identifier, password)
-		user = user.find_by_username(identifier) || user.find_by_email(identifier)
-		return nil unless user && user.valid_password(password)
+		user = User.find_by_username(identifier) || User.find_by_email(identifier)
+		return nil unless user && user.valid_password?(password)
 		user
 	end
 
