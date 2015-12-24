27c43bf6f8e8bd9fcf5a61393c049dfe835dcbcf
Cassie Montoya <clm21020@gmail.com>
2015-12-23 16:22:54 -0800

    Add already_logged_in before_action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
+	before_action :already_logged_in
+
 	def welcome
 	end
+
+	def already_logged_in
+    redirect_to items_url if logged_in?
+  end
 end
